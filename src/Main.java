05f1da3ff63d599728f1b0edeb6a540f81423842
Minh Vu <minhd_vu@yahoo.com>
2019-10-03 14:44:15 -0400

    display a screen!

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-
+        primaryStage.setTitle("Othello Java - Dylan Jones, Minh Vu");
+        primaryStage.show();
     }
 }
