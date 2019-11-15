a62391516dffb93b26f9d4ac38f0b2a4bd57999b
Dylan Jones <dylanjones2011@gmail.com>
2019-11-15 16:50:48 -0500

    ADD LEGAL MOVES

--- src/engine/State.java
+++ src/engine/State.java
@@ -1,6 +1,7 @@
 package engine;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class State implements Serializable {
     // Weight matrix for a basic heuristic function.
@@ -15,26 +16,26 @@ public class State implements Serializable {
             120, -20, 20, 5, 5, 20, -20, 120
     };
 
-    public static final int EMPTY = 0;
-    public static final int BLACK = 1;
-    public static final int WHITE = 2;
+    public static final byte EMPTY = 0;
+    public static final byte BLACK = 1;
+    public static final byte WHITE = 2;
 
     // Bitboard representation - the only fields on this object should be black and white bitmasks and the moving player
-    public final long ourMask;
-    public final long theirMask;
-    public int movingColor;
+    public long blackMask;
+    public long whiteMask;
+    public byte movingColor;
     public long moveMask;
 
-    public State(long ourMask, long theirMask, int lastPlayer) {
-        this.ourMask = ourMask;
-        this.theirMask = theirMask;
+    public State(long blackMask, long whiteMask, byte lastPlayer) {
+        this.blackMask = blackMask;
+        this.whiteMask = whiteMask;
         this.movingColor = invert(lastPlayer);
-        moveMask = calcLegalMoves(movingColor);
+        moveMask = calcLegalMoves();
         if (moveMask == 0) {
             movingColor = lastPlayer;
-            moveMask = calcLegalMoves(movingColor);
+            moveMask = calcLegalMoves();
             if (moveMask == 0) {
-                movingColor = EMPTY;
+                movingColor = 0;
             }
         }
     }
@@ -48,15 +49,23 @@ public class State implements Serializable {
     public int[] toBoxBoard() {
         int[] board = new int[64];
         for (int i = 0; i < 64; i++) {
-            if ((ourMask >>> i & 1) == 1) {
-                board[i] = movingColor;
-            } else if ((theirMask >>> i & 1) == 1) {
-                board[i] = invert(movingColor);
+            if (((blackMask >>> i) & 1) == 1) {
+                board[i] = BLACK;
+            } else if (((whiteMask >>> i) & 1) == 1) {
+                board[i] = WHITE;
             } else {
                 board[i] = EMPTY;
             }
         }
         return board;
+    }
+
+    public long getOurMask() {
+        return movingColor == BLACK ? blackMask : whiteMask;
+    }
+
+    public long getTheirMask() {
+        return movingColor == BLACK ? whiteMask : blackMask;
     }
 
 
@@ -65,12 +74,14 @@ public class State implements Serializable {
      *
      * @return a bitmask with valid moves as ones.
      */
-    public long calcLegalMoves(int player) {
-        return (getMovesPartial(ourMask, theirMask, 1) |  // horizontal
-                getMovesPartial(ourMask, theirMask, 8) | // vertical
-                getMovesPartial(ourMask, theirMask, 7) | // diagonal like /
-                getMovesPartial(ourMask, theirMask, 9)) & // diagonal like \
-                ~(ourMask | theirMask); // mask out to only the empty squares
+    public long calcLegalMoves() {
+        long us = getOurMask();
+        long them = getTheirMask();
+        return (getMovesPartial(us, them & 0x7E7E7E7E7E7E7E7EL, 1) | // horizontal
+                getMovesPartial(us, them & 0x00FFFFFFFFFFFF00L, 8) | // vertical
+                getMovesPartial(us, them & 0x007E7E7E7E7E7E00L, 7) | // diagonal like /
+                getMovesPartial(us, them & 0x007E7E7E7E7E7E00L, 9)) & // diagonal like \
+                ~(us | them); // mask out to only the empty squares
     }
 
     /**
@@ -79,59 +90,85 @@ public class State implements Serializable {
      * Based on Edax's implementation here https://github.com/abulmo/edax-reversi/blob/master/src/board.c#L571
      */
     private long getMovesPartial(long p1, long mask, int dir) {
-        long flip;
-        flip = ((p1 << dir | p1 >>> dir) & mask); // initialize w/ one smear
-        // manually unrolled loop bc "efficiency"
-        flip |= (((flip << dir) | (flip >>> dir)) & mask); // do another 5 smears here
-        flip |= (((flip << dir) | (flip >>> dir)) & mask);
-        flip |= (((flip << dir) | (flip >>> dir)) & mask);
-        flip |= (((flip << dir) | (flip >>> dir)) & mask);
-        flip |= (((flip << dir) | (flip >>> dir)) & mask);
+        long flip = ((p1 << dir) | (p1 >>> dir)) & mask;
+        for (int i = 0; i < 6; i++) {
+            flip |= (((flip << dir) | (flip >>> dir)) & mask); // smear 6 times _inside_ the bounds
+        }
         return (flip << dir) | (flip >>> dir); // final smear isn't masked to allow it to go outside the bounds
+    }
+
+    /**
+     * Same as getMovesPartial, but only does left shifts.
+     *
+     * @param p1   the piece to start at
+     * @param mask the opponent's pieces
+     * @param dir  the direction to fill in
+     * @return the flooded board
+     */
+    private long getMovesPartialLeft(long p1, long mask, int dir) {
+        long flip = p1 << dir & mask;
+        for (int i = 0; i < 6; i++) {
+            flip |= flip << dir & mask; // smear 6 times _inside_ the bounds
+        }
+        return flip << dir; // final smear isn't masked to allow it to go outside the bounds
+    }
+
+    /**
+     * Same as getMovesPartial, but only does right shifts.
+     *
+     * @param p1   the piece to start at
+     * @param mask the opponent's pieces
+     * @param dir  the direction to fill in
+     * @return the flooded board
+     */
+    private long getMovesPartialRight(long p1, long mask, int dir) {
+        long flip = p1 >>> dir & mask;
+        for (int i = 0; i < 6; i++) {
+            flip |= flip >>> dir & mask; // smear 6 times _inside_ the bounds
+        }
+        return flip >>> dir; // final smear isn't masked to allow it to go outside the bounds
     }
 
     /* HEURISTIC FUNCTIONS */
     public int weightSquare() {
         int out = 0;
+        long us = getOurMask();
+        long them = getTheirMask();
         for (int i = 0; i < 64; i++) {
-            if ((ourMask << i & 1) == 1) {
+            if ((us << i & 1) == 1) {
                 out += SQUARE_WEIGHTS[i];
-            } else if ((theirMask << i & 1) == 1) {
+            } else if ((them << i & 1) == 1) {
                 out -= SQUARE_WEIGHTS[i];
             }
         }
         return out;
     }
 
-
     // util methods
 
     /**
      * @return the inverse of the given color.
      */
-    public static int invert(int color) {
+    public static byte invert(byte color) {
         return color == BLACK ? WHITE : BLACK;
     }
 
+    /**
+     * Get all successors of the current node.
+     *
+     * @return an array with all children of this node.
+     */
     public State[] getChildren() {
         State[] c = new State[Long.bitCount(moveMask)];
         long moves = moveMask;
-        for (int i = 0; moves != 0; ) {
+        int moveIdx = 0;
+        for (int i = 0; moves != 0; i++) {
             while ((moves & 1) == 0) {
                 moves >>>= 1;
-                i++;
+                moveIdx++;
             }
             moves >>>= 1;
-            // i is the index of the move
-            long piece = 1L << i;
-            long toFlip = (getMovesPartial(piece, theirMask, 1) |  // horizontal
-                    getMovesPartial(piece, theirMask, 8) | // vertical
-                    getMovesPartial(piece, theirMask, 7) | // diagonal like /
-                    getMovesPartial(piece, theirMask, 9)) & // diagonal like \
-                    (piece | theirMask); // mask out to only the other player's squares and our move
-            long them = theirMask & ~toFlip;
-            long us = ourMask | toFlip;
-
+            c[i] = makeMove(moveIdx);
         }
         if (true) {
             throw new RuntimeException();
@@ -139,10 +176,65 @@ public class State implements Serializable {
         return c;
     }
 
+    public State makeMove(int idx) {
+        long flipped = 0;
+        long piece = 1L << idx;
+        long them = getTheirMask();
+        long empty = ~(blackMask | whiteMask);
+        // figure out what flips when we make a move
+        {
+            // diagonal up left
+            long tmp = getMovesPartialRight(piece, them, 9);
+            if ((tmp & empty) != 0) {
+                flipped |= tmp;
+            }
+            // up
+            tmp = getMovesPartialRight(piece, them, 8);
+            if ((tmp & empty) != 0) {
+                flipped |= tmp;
+            }
+            // diagonal up right
+            tmp = getMovesPartialRight(piece, them, 7);
+            if ((tmp & empty) != 0) {
+                flipped |= tmp;
+            }
+            // left
+            tmp = getMovesPartialRight(piece, them, 1);
+            if ((tmp & empty) != 0) {
+                flipped |= tmp;
+            }
+            // right
+            tmp = getMovesPartialLeft(piece, them, 1);
+            if ((tmp & empty) != 0) {
+                flipped |= tmp;
+            }
+            // diagonal down left
+            tmp = getMovesPartialLeft(piece, them, 7);
+            if ((tmp & empty) != 0) {
+                flipped |= tmp;
+            }
+            // down
+            tmp = getMovesPartialLeft(piece, them, 8);
+            if ((tmp & empty) != 0) {
+                flipped |= tmp;
+            }
+            // diagonal down right
+            tmp = getMovesPartialLeft(piece, them, 9);
+            if ((tmp & empty) != 0) {
+                flipped |= tmp;
+            }
+        }
+
+        // actually do the flips to their pieces
+        them = them & ~flipped;
+        long us = getOurMask() | flipped;
+        return new State(movingColor == BLACK ? us : them, movingColor == WHITE ? us : them, movingColor);
+    }
+
     /**
      * Create a new board from an array board, automatically converting it to a bitboard.
      */
-    public static State fromBoxBoard(int[] intBoard, int lastPlayer) {
+    public static State fromBoxBoard(int[] intBoard, byte lastPlayer) {
         long black = 0;
         long white = 0;
         for (int i = 0; i < 64; i++) {
@@ -172,9 +264,22 @@ public class State implements Serializable {
     }
 
     // TESTING ONLY
+
+    private static void printLong(long l) {
+        State s = new State(l, l, WHITE);
+        s.movingColor = BLACK;
+        System.out.println(l);
+//        System.out.println(Arrays.toString(s.toBoxBoard()));
+        System.out.println(s);
+        System.out.println();
+    }
+
     public static void main(String... args) {
         // starting board
         State s = new State(0b00000000000000000001000000001000000000000000000000000000L, 0b00000000000000000000100000010000000000000000000000000000L, BLACK);
+        s.blackMask = 0b00000000000000000001000000001000000000000000000000000000L;
+        s.whiteMask = 0b00000000000000000000100000010000000000000000000000000000L;
+        s.movingColor = BLACK;
 //        System.out.println(s);
 //        System.out.println(Long.toBinaryString(s.black));
         s = State.fromBoxBoard(new int[]{
@@ -186,11 +291,9 @@ public class State implements Serializable {
                 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0,
-        }, BLACK);
+        }, WHITE);
         System.out.println(s);
         System.out.println();
-        System.out.println(new State(s.calcLegalMoves(BLACK), 0, WHITE));
-//        System.out.println();
-//        System.out.println(Arrays.toString(s.getChildren()));
+        printLong(s.calcLegalMoves());
     }
 }
